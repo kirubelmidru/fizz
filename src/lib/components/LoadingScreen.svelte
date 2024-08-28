53f6b1a02c45bec7fd8748be5ae95c1425e6c400
Kirubel Midru <kirubelmidru@gmail.com>
2024-08-28 21:20:47 +0300

    loading screen fix

--- src/lib/components/LoadingScreen.svelte
+++ src/lib/components/LoadingScreen.svelte
@@ -3,31 +3,55 @@ import { fade } from 'svelte/transition';
 import { quadInOut } from 'svelte/easing';
 </script>
 
-<div class="w-screen h-screen flex overflow-hidden bg-chinese-black z-[999]" out:fade>
-    <div class="wave w-screen h-screen"/>
+<div class="w-screen h-screen flex bg-chinese-black z-[999]" out:fade>
+    <div class="inner-wave w-full h-full"/>
+    <div class="wave w-full h-full"/>
 </div>
 
 <style>
-
-.wave {
-    position: relative;
-    opacity: 0;
-    border: #FFF 20px solid;
+.inner-wave {
+    position: absolute;
+    transform: scale(0);
+    border: #FFF 5px solid;
     border-radius: 100%;
-    -moz-animation-name: ripple 2s linear infinite 2s;
-    -webkit-animation: ripple 2s linear infinite 2s;
-    animation: ripple 2s linear infinite 2s;
+    -moz-animation-name: inner-ripple 2s infinite;
+    -webkit-animation: inner-ripple 2s infinite;
+    animation: inner-ripple 2s infinite;
+}
+.wave {
+    position: absolute;
+    transform: scale(0);
+    border-width:#FFF 7px solid;
+    border-radius: 100%;
+    -moz-animation-name: ripple 2s infinite;
+    -webkit-animation: ripple 2s infinite;
+    animation: ripple 2s infinite;
 }
 
+
 @keyframes ripple {
-    from {
+    0% {
 	opacity: 1;
-	transform: scale(0);
     }
-    to {
-	transform: scale(0.2);
+    100% {
+	transform: scale(0.3);
 	border-width: 1px;
 	opacity: 0;
     }
 }
+
+@keyframes inner-ripple {
+    0% {
+	opacity: 1;
+    }
+    50% {
+	opacity: 0;
+    }
+    100% {
+	opacity: 0;
+	transform: scale(0.1);
+	border-width: 1px;
+    }
+}
+
 </style>
